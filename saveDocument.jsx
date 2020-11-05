86567650bb1a245f1f817c5b2d75d930f309fcc2
Gregor Fellenz <gregor.fellenz@publishingx.de>
2020-11-05 11:51:23 +0100

    fix InDesing CC 21 save bug

---
+++ saveDocument.jsx
@@ -0,0 +1,20 @@
+﻿//DESCRIPTION:Fix InDesign CC 2021 Bug while saving old documents. 
+//Contact: Gregor Fellenz - http://www.publishingx.de
+
+if (app.documents.length > 0 && app.activeDocument.modified == true) { 
+    try {
+        var dok = app.activeDocument;
+        var path = dok.filePath;
+        var saveFile = File (path + "/" + dok.name);
+        dok.save(saveFile);    
+    }
+    catch(e) {
+        if (e.number == 90937) {
+            // never saved, no filePath -> User selects
+            dok.save();
+        }    
+        else {
+            alert(e);
+        }
+    }
+} 
